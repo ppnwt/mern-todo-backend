33c1837721720fc103984a11845174e622978a94
New Panuwat <pfcreatorit@gmail.com>
2019-06-29 22:07:21 +0700

    setting up mongodb heroku

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const todoRoutes = express.Router();
-const PORT = 4000
+const PORT = process.env.PORT || 4000
 
 let Todo = require('./todo.model')
 
 app.use(cors())
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
 const connection = mongoose.connection
 
 connection.once('open', function() {
